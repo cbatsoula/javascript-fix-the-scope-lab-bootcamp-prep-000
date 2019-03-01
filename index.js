16fac1eb3df874a20ae18a9da2d148c7871fdf11
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-01 21:19:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 function theFunk() {
-  return funkyFunction
+  return function() {
+    return "FUNKY!"
+  }
 }
